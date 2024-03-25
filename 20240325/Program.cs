d417bc59501120dd14db5d4c4f884eb3244321f7
Denny Chan <dennychan956@gmail.com>
2024-03-25 12:20:55 +0800

    填充空白

--- 20240325/Program.cs
+++ 20240325/Program.cs
@@ -1,6 +1,12 @@
 ﻿// 輸出星期
 Console.WriteLine($"日 一 二 三 四 五 六");
 
+// 填充空白
+for (int space = 1; space <= 5; space = space + 1)
+{
+    Console.Write(" ");
+}
+
 // 輸出月曆內容
 for (int i = 1; i <= 31; i = i + 1)
 {

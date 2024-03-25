da8d7530068b13518a777205a2faff3ad48645d2
Denny Chan <dennychan956@gmail.com>
2024-03-25 12:37:26 +0800

    計算當月的天數

--- 20240325/Program.cs
+++ 20240325/Program.cs
@@ -5,6 +5,9 @@ Console.WriteLine($"日\t一\t二\t三\t四\t五\t六");
 DateTime firstDayOfMonth = new DateTime(2024, 3, 1);
 int firstDayOfWeek = (int)firstDayOfMonth.DayOfWeek;
 
+// 計算當月的天數
+int daysInMonth = DateTime.DaysInMonth(2024, 3);
+
 // 填充空白
 for (int space = 1; space <= firstDayOfWeek; space = space + 1)
 {
@@ -12,7 +15,7 @@ for (int space = 1; space <= firstDayOfWeek; space = space + 1)
 }
 
 // 輸出月曆內容
-for (int i = 1; i <= 31; i = i + 1)
+for (int i = 1; i <= daysInMonth; i = i + 1)
 {
     Console.Write(i);
     if ((5 + i) % 7 == 0)

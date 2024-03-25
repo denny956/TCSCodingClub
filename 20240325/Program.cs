521f7c4623a20f0daad95c16bbfd63c21f30d48f
Denny Chan <dennychan956@gmail.com>
2024-03-25 12:36:44 +0800

    計算當月第一天的星期

--- 20240325/Program.cs
+++ 20240325/Program.cs
@@ -1,8 +1,12 @@
 ﻿// 輸出星期
 Console.WriteLine($"日\t一\t二\t三\t四\t五\t六");
 
+// 計算當月第一天的星期
+DateTime firstDayOfMonth = new DateTime(2024, 3, 1);
+int firstDayOfWeek = (int)firstDayOfMonth.DayOfWeek;
+
 // 填充空白
-for (int space = 1; space <= 5; space = space + 1)
+for (int space = 1; space <= firstDayOfWeek; space = space + 1)
 {
     Console.Write("\t");
 }
